595cace00b65dbfa0dca858a1fae2d283c29835a
jvitor <jvitor.pinheiro.garcia@gmail.com>
2025-03-10 15:18:14 -0300

    Iniciando Projeto

---
+++ src/main/java/com/senai/exercicioExtra/ExercicioExtraApplication.java
@@ -0,0 +1,13 @@
+package com.senai.exercicioExtra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioExtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioExtraApplication.class, args);
+	}
+
+}
